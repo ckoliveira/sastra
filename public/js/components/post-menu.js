4844cc3324f56841d59d20ca39e0ea9a66d1c3a6
Patrick <erick.nascol@gmail.com>
2025-07-11 22:22:11 -0300

    improve post menu

--- public/js/components/post-menu.js
+++ public/js/components/post-menu.js
@@ -6,18 +6,24 @@ export const POST_BODY_INPUT = "post-body";
 export const POST_TAGS_INPUT = "post-tags";
 export const PostMenu = () => `
     <section id=${POST_MENU_DIV}>
-        <header class="post-menu-actions">
-            <button id=${POST_MENU_SAVE_BUTTON}>Save Entry</button>
-            <button id=${POST_MENU_CLOSE_BUTTON}>Close Menu</button>
-        </header>
-        
-        <input type="text" placeholder="Title" id=${POST_TITLE_INPUT} />
+        <div class="post-menu-wrapper">
+            <header class="post-menu-actions">
+                <button id=${POST_MENU_SAVE_BUTTON}>Save Entry</button>
+                <button id=${POST_MENU_CLOSE_BUTTON}>Close Menu</button>
+            </header>
+            
+            <input type="text" placeholder="Title" id=${POST_TITLE_INPUT} />
 
-        <textarea
-            id=${POST_BODY_INPUT}
-            placeholder="Enter your text here">
-        </textarea>
+            <textarea
+                id=${POST_BODY_INPUT}
+                placeholder="Enter your text here"></textarea>
 
-        <input type="text" id=${POST_TAGS_INPUT} placeholder="Tags" />
+            <input type="text" id=${POST_TAGS_INPUT} placeholder="Tags" />
+        </div>
+
+        <div class="post-menu-sastra-logo">
+            <h1>S A S T R A</h1>
+            <img src="/images/round_logo.png" alt="sastra logo">
+        </div>
     </section>
 `;
